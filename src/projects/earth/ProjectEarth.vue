6791773433bcc44338dd73072a00f4b0e84c85cf
Sergio Juan Armero <sjarmero@gmail.com>
2026-04-06 15:31:17 +0200

    feat(earth): add textures

--- src/projects/earth/ProjectEarth.vue
+++ src/projects/earth/ProjectEarth.vue
@@ -9,13 +9,22 @@ import {
   MeshStandardMaterial,
   PerspectiveCamera,
   Scene,
-  SphereGeometry
+  SphereGeometry, TextureLoader
 } from 'three';
 import SceneViewer from '@/components/scene-viewer/scene-viewer.vue';
 import {OrbitControls} from 'three/addons/controls/OrbitControls';
 import {onMounted} from 'vue';
+import {useLoadingManager} from '@/components/scene-viewer/useLoadingManager.ts';
+
+import earthColorTexturePath from '~/assets/earth/8k_earth_daymap.jpg';
+import earthNormalTexturePath from '~/assets/earth/8k_earth_normal_map.tif?url';
+import {TIFFLoader} from 'three/addons/loaders/TIFFLoader';
 
 // 1 unit: 1000 KM
+
+const loadingManager = useLoadingManager();
+const textureLoader = new TextureLoader(loadingManager);
+const tiffLoader = new TIFFLoader(loadingManager);
 
 const context = use3DContext();
 context.scene = new Scene();
@@ -38,9 +47,15 @@ function setCamera() {
 function addEarth() {
   const earth = new Group();
 
+  const earthColorTexture = textureLoader.load(earthColorTexturePath);
+  const earthNormalTexture = tiffLoader.load(earthNormalTexturePath);
+
   const globe = new Mesh(
-    new SphereGeometry(6, 64, 64),
-    new MeshStandardMaterial()
+    new SphereGeometry(6, 128, 128),
+    new MeshStandardMaterial({
+      map: earthColorTexture,
+      normalMap: earthNormalTexture,
+    })
   );
   earth.add(globe);
 
